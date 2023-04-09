36e836083d8e25733f0875568fda25bac49a4fe7
veena ambalavanan <veenaa014@gmail.com>
2023-04-09 15:28:56 -0500

    created graphql schema using type-graphql, setup express

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,38 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constant";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   const emFork = orm.em.fork(); // create the fork of new EntityManager instance
   await orm.getMigrator().up(); // run the Migration question? not sure if emFork.getMigrator().up() or orm
 
-  // const post = emFork.create(Post, { title: "my first post" }); //use the fork instead of global `orm.em`, as we don't want to change global EntityManager em instance
-  // await emFork.persistAndFlush(post);
+  const app = express();
+  const port = 3000;
+  //rest endpoint
+  // app.get("/", (_, res) => {
+  //   res.send("Hello world");
+  // });
 
-  // const posts = await emFork.find(Post, {});
-  // console.log(posts);
+  //graphql
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  //create graphql endpoint on express
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(port, () => {
+    console.log(`App listening on ${port}`);
+  });
 };
 
 main().catch((err) => {
