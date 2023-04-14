79c39c7b31019176c618b28d0846196c5c85f965
veena ambalavanan <veenaa014@gmail.com>
2023-04-14 00:52:52 -0500

    update and delete posts

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -13,20 +13,56 @@ export class PostResolver {
   //read post for given id - return null otherwise
   @Query(() => Post, { nullable: true }) // graphql return type Post | null return type
   post(
-    @Arg("id", () => Int) id: number,  //no need to explicitly mention () => Int, typegraphql can infer int, strings 
+    @Arg("id", () => Int) id: number, //no need to explicitly mention () => Int, typegraphql can infer int, strings
     @Ctx() { em }: MyContext
-  ): Promise<Post | null> { //typescript type
+  ): Promise<Post | null> {
+    //typescript type
     return em.findOne(Post, { id });
   }
   //create
   @Mutation(() => Post) // graphql return type Post | null return type
   async createPost(
-    @Arg("title", ()=> String) title: string, // String(wrapper object) is a different type from string(primitive datatype) here, use of ()=> SString is optional, typegraphql can infer without it 
+    @Arg("title", () => String) title: string, // String(wrapper object) is a different type from string(primitive datatype) here, use of ()=> SString is optional, typegraphql can infer without it
     @Ctx() { em }: MyContext
-  ): Promise<Post> { //typescript type
-    const post = em.create(Post, {title})
-    await em.persistAndFlush(post)
+  ): Promise<Post> {
+    //typescript type
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
     return post;
   }
 
+  //update
+  @Mutation(() => Post, { nullable: true }) // graphql return type Post | null return type
+  async updatePost(
+    @Arg("title", () => String, { nullable: true }) title: string, // ()=> String, {nullable:true} sets the title to string or null as input
+    @Arg("id") id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    //typescript type
+    const post = await em.findOne(Post, { id });
+    if (!post) {
+      return null;
+    }
+    if (typeof title !== "undefined") {
+      //since title can be null
+      post.title = title;
+      await em.persistAndFlush(post);
+    }
+    return post;
+  }
+
+  //delete
+  @Mutation(() => Boolean) // graphql return type Post | null return type
+  async deletePost(
+    @Arg("id") id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Boolean> {
+    //typescript type
+    try {
+      await em.nativeDelete(Post, { id });
+      return true;
+    } catch {
+      return false;
+    }
+  }
 }
