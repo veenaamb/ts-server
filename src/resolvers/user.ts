d05d422770d3ad734122c022e9b074dc2d16754b
veena ambalavanan <veenaa014@gmail.com>
2023-05-04 23:26:33 -0500

    added logout

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -112,4 +112,21 @@ export class UserResolver {
 
     return { user };
   }
+
+  @Mutation(() => Boolean)
+  logout(@Ctx() { req, res }: MyContext) {
+    return new Promise((resolve) =>
+      req.session.destroy((err) => {
+        res.clearCookie(COOKIE_NAME);
+        if (err) {
+          console.log(err);
+          resolve(false);
+          return;
+        }
+
+        resolve(true);
+      })
+    );
+  }
 }
+
